02552039ac7c0c138d48163fb6bbd77916b3038b
Johan Palacios <johanpalacios.rivera@gmail.com>
2023-01-11 22:19:48 -0600

    Add Configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      ...colors,
+    },
+  },
+  plugins: [],
+};
