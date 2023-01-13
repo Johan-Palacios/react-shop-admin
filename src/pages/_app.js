8ba828a73571dc3858213c745cb1188c8b05378c
Johan Palacios <johanpalacios.rivera@gmail.com>
2023-01-12 21:58:12 -0600

    ADD Components

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
+import MainLayout from "@layout/MainLayout";
 import "@styles/tailwind.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </>
+  );
 }
