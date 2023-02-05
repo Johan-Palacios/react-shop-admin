191e3d44bc26018da9f734214401d3de52cf0a09
Johan Palacios <johanpalacios.rivera@gmail.com>
2023-02-04 23:15:31 -0600

    Add Next link to dashboard

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import useFetch from "@hooks/useFetch";
 import endPoints from "@services/api";
 import { useState } from "react";
@@ -118,12 +119,12 @@ export default function Dashboard() {
                         {product.id}
                       </td>
                       <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                        <a
+                        <Link
                           href="/edit"
                           className="text-indigo-600 hover:text-indigo-900"
                         >
                           Edit
-                        </a>
+                        </Link>
                       </td>
                       <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                         <a
