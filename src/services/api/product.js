03c0ef9bfa82b01d165766c7010be2dff9495ccc
Johan Palacios <johanpalacios.rivera@gmail.com>
2023-02-04 23:14:17 -0600

    Add Update product to api manager

--- src/services/api/product.js
+++ src/services/api/product.js
@@ -16,9 +16,24 @@ const addProduct = async (body) => {
   return response.data;
 };
 
+const updateProduct = async (id, body) => {
+  const config = {
+    headers: {
+      accept: "*/*",
+      "Content-Type": "application/json",
+    },
+  };
+  const response = await axios.put(
+    endPoints.products.updateProducts(id),
+    body,
+    config
+  );
+  return response.data;
+};
+
 const deleteProduct = async (id) => {
   const response = await axios.delete(endPoints.products.deleteProduct(id));
   return response.data;
 };
 
-export { addProduct, deleteProduct };
+export { addProduct, deleteProduct, updateProduct };
